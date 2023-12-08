961d754690e9eb277cdc377b2ec6adc1d279c345
Aleksey2202 <144618320+Aleksey2202@users.noreply.github.com>
2023-12-08 13:57:26 +0300

    Add files via upload

---
+++ lesson_17/js/script.js
@@ -0,0 +1,13 @@
+const icon = document.querySelector('.header__burger');
+icon.addEventListener('click', function() {
+	document.documentElement.classList.toggle('active');
+});
+
+
+
+
+// $(document).ready(function() {
+// 	$('.header__burger').click(function(event) {
+// 		$('.header__burger, .header__menu').toggleClass('active');
+// 	});
+// });
