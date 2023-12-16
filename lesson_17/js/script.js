5ecaaa6c2f06ce649834ce95cd7d9f5043a93b8e
Aleksey2202 <144618320+Aleksey2202@users.noreply.github.com>
2023-12-16 11:19:56 +0300

    Delete lesson_17 directory

--- lesson_17/js/script.js
+++
@@ -1,8 +0,0 @@
-const icon = document.querySelector('.header__burger');
-icon.addEventListener('click', function() {
-	document.documentElement.classList.toggle('active');
-});
-const scroll = document.querySelector('body');
-icon.addEventListener('click', function() {
-	document.documentElement.classList.toggle('lock');
-});
